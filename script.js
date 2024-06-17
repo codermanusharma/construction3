8962aabb80ac1ce3bad03622008e5aa5c8038c6b
codermanusharma <145802851+codermanusharma@users.noreply.github.com>
2024-06-17 16:22:41 +0530

    construction

---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function() {
+    (function(){
+        emailjs.init("TCzTJZ3RS0yrb3OY0"); // Replace 'YOUR_USER_ID' with your EmailJS user ID
+    })();
+
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        // Change these to your EmailJS service ID, template ID, and your user ID
+        const serviceID = 'service_mjxi3d4';
+        const templateID = 'template_jw44278';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                document.getElementById('status').innerText = 'Message sent successfully!';
+            }, (err) => {
+                document.getElementById('status').innerText = 'Failed to send message. Please try again.';
+                console.log(JSON.stringify(err));
+            });
+    });
+});
+
+// smooth scrolling 
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Smooth Scrolling
+document.querySelectorAll('.nav-menu a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+
+        // Hide menu after clicking a link (for mobile view)
+        document.getElementById('nav-menu').classList.remove('show');
+    });
+});
+
+// Toggle Menu
+const menuIcon = document.getElementById('menu-icon');
+const navMenu = document.getElementById('nav-menu');
+
+menuIcon.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
